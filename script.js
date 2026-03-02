4a9fcdad42497a094c4eae33c66c3dc370febe10
D45-csm <acos79.0.98dan1lec@gmail.com>
2026-03-02 17:42:58 -0500

    Update script.js
    AGREGO FUNCION AL PUNTERO

---
+++ script.js
@@ -0,0 +1,9 @@
+/* puntero*/
+const puntero= document.getElementById("punto");
+
+window.addEventListener("mousemove",e=>{
+mouseX = e.clientX;
+mouseY = e.clientY;
+puntero.style.left = mouseX + 'px';
+puntero.style.top = mouseY + 'px';
+})
