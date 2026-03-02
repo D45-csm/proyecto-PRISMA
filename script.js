8783f737ec476f065b0234e4ca42ec9502ebf47f
D45-csm <acos79.0.98dan1lec@gmail.com>
2026-03-02 11:53:00 -0500

    Update script.js
    puse movimiento al puntero

--- script.js
+++ script.js
@@ -15,6 +15,18 @@ window.addEventListener("scroll", function(){
     }
 })
 
+/* puntero*/
+const puntero= document.getElementById("punto");
+
+window.addEventListener("mousemove",e=>{
+mouseX = e.clientX;
+mouseY = e.clientY;
+puntero.style.left = mouseX + 'px';
+puntero.style.top = mouseY + 'px';
+})
+
+
+
 
 
 
