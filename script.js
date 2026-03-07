642103f17db0c39f3f3a117cd8af702ec7ea656f
DanielaRodriguez <daniiela.rodriguez112@gmail.com>
2026-03-07 17:34:11 -0500

    nuevo script

--- script.js
+++ script.js
@@ -9,16 +9,3 @@ mouseY = e.clientY;
 puntero.style.left = mouseX + 'px';
 puntero.style.top = mouseY + 'px';
 })
-
-/* barra progreso */
-const barraArriba = document.getElementById("progress-bar")
-
-function barraProgreso(){
-    const alturaPantalla = window.scrollY;
-    const restaAltura = document.documentElement.scrollHeight - window.innerHeight;
-    const  porcentaje = ( alturaPantalla / restaAltura) *100;
-    barraArriba.style.width = porcentaje + "%"
-}
-
-window.addEventListener("scroll", barraProgreso)
-
